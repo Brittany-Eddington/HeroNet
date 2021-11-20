ade9b856c24d86fdcd59acf6fc10e469916a7d99
Brittany Eddington <57241336+Brittany-Eddington@users.noreply.github.com>
2021-11-20 02:31:08 -0700

    Add files via upload

---
+++ HeroNet/HeroNet.js
@@ -0,0 +1,139 @@
+document.getElementById("btnMain").onclick=function(){
+    document.location.href="main.html";
+}
+
+document.getElementById("btnAuthor").onclick=function(){
+    document.location.href="author.html";
+}
+
+document.getElementById("btnCharBio").onclick=function(){
+    document.location.href="charBio.html";
+}
+
+document.getElementById("btnShop").onclick=function(){
+    document.location.href="shop.html";
+}
+
+$("#HN").click(function(){
+    $(".bodyHN, .bodyJG, .bodyWK, .bodyTS, .bodyV, .bodyBQ, .bodyTB").removeClass().addClass("bodyHN");
+    $(".topHN, .topJG, .topWK, .topTS, .topV, .topBQ, .topTB").removeClass().addClass("topHN");
+    $(".navHN, .navJG, .navWK, .navTS, .navV, .navBQ, .navTB").removeClass().addClass("navHN");
+    $(".buttonsHN, .buttonsJG, .buttonsWK, .buttonsTS, .buttonsV, .buttonsBQ, .buttonsTB").removeClass().addClass("buttonsHN");
+    $(".footHN, .footJG, .footWK, .footTS, .footV, .footBQ, .footTB").removeClass().addClass("footHN");
+    $(".smallTextHN, .smallTextJG, .smallTextWK, .smallTextTS, .smallTextV, .smallTextBQ, .smallTextTB").removeClass().addClass("smallTextHN");
+    $(".mainBioHN, .mainBioJG, .mainBioWK, .mainBioTS, mainBioV, mainBioBQ, mainBioTB").removeClass().addClass("mainBioHN");
+    $(".bioBtnHN, .bioBtnJG, .bioBtnWK, .bioBtnTS, .bioBtnV, .bioBtnBQ, .bioBtnTB").removeClass().addClass("bioBtnHN");
+    $(".mainHN, .mainJG, .mainWK, .mainTS, .mainV, .mainBQ, .mainTB").removeClass().addClass("mainHN");
+    $(".authorPicHN, .authorPicJG, .authorPicWK, .authorPicTS, .authorPicV, .authorPicBQ, .authorPicTB").removeClass().addClass("authorPicHN");
+    $(".authorWordsHN, .authorWordsJG, .authorWordsWK, .authorWordsTS, .authorWordsV, .authorWordsBQ, .authorWordsTB").removeClass().addClass("authorWordsHN");
+    $(".shopHN, .shopJG, .shopWK, .shopTS, .shopV, .shopBQ, .shopTB").removeClass().addClass("shopHN");
+});
+$("#JG").click(function(){
+    $(".bodyHN, .bodyJG, .bodyWK, .bodyTS, .bodyV, .bodyBQ, .bodyTB").removeClass().addClass("bodyJG");
+    $(".topHN, .topJG, .topWK, .topTS, .topV, .topBQ, .topTB").removeClass().addClass("topJG");
+    $(".navHN, .navJG, .navWK, .navTS, .navV, .navBQ, .navTB").removeClass().addClass("navJG");
+    $(".buttonsHN, .buttonsJG, .buttonsWK, .buttonsTS, .buttonsV, .buttonsBQ, .buttonsTB").removeClass().addClass("buttonsJG");
+    $(".footHN, .footJG, .footWK, .footTS, .footV, .footBQ, .footTB").removeClass().addClass("footJG");
+    $(".smallTextHN, .smallTextJG, .smallTextWK, .smallTextTS, .smallTextV, .smallTextBQ, .smallTextTB").removeClass().addClass("smallTextJG");
+    $(".mainBioHN, .mainBioJG, .mainBioWK, .mainBioTS, mainBioV, mainBioBQ, mainBioTB").removeClass().addClass("mainBioJG");
+    $(".bioBtnHN, .bioBtnJG, .bioBtnWK, .bioBtnTS, .bioBtnV, .bioBtnBQ, .bioBtnTB").removeClass().addClass("bioBtnJG");
+    $(".mainHN, .mainJG, .mainWK, .mainTS, .mainV, .mainBQ, .mainTB").removeClass().addClass("mainJG");
+    $(".authorPicHN, .authorPicJG, .authorPicWK, .authorPicTS, .authorPicV, .authorPicBQ, .authorPicTB").removeClass().addClass("authorPicJG");
+    $(".authorWordsHN, .authorWordsJG, .authorWordsWK, .authorWordsTS, .authorWordsV, .authorWordsBQ, .authorWordsTB").removeClass().addClass("authorWordsJG");
+    $(".shopHN, .shopJG, .shopWK, .shopTS, .shopV, .shopBQ, .shopTB").removeClass().addClass("shopJG");
+});
+$("#WK").click(function(){
+    $(".bodyHN, .bodyJG, .bodyWK, .bodyTS, .bodyV, .bodyBQ, .bodyTB").removeClass().addClass("bodyWK");
+    $(".topHN, .topJG, .topWK, .topTS, .topV, .topBQ, .topTB").removeClass().addClass("topWK");
+    $(".navHN, .navJG, .navWK, .navTS, .navV, .navBQ, .navTB").removeClass().addClass("navWK");
+    $(".buttonsHN, .buttonsJG, .buttonsWK, .buttonsTS, .buttonsV, .buttonsBQ, .buttonsTB").removeClass().addClass("buttonsWK");
+    $(".footHN, .footJG, .footWK, .footTS, .footV, .footBQ, .footTB").removeClass().addClass("footWK");
+    $(".smallTextHN, .smallTextJG, .smallTextWK, .smallTextTS, .smallTextV, .smallTextBQ, .smallTextTB").removeClass().addClass("smallTextWK");
+    $(".mainBioHN, .mainBioJG, .mainBioWK, .mainBioTS, mainBioV, mainBioBQ, mainBioTB").removeClass().addClass("mainBioWK");
+    $(".bioBtnHN, .bioBtnJG, .bioBtnWK, .bioBtnTS, .bioBtnV, .bioBtnBQ, .bioBtnTB").removeClass().addClass("bioBtnWK");
+    $(".mainHN, .mainJG, .mainWK, .mainTS, .mainV, .mainBQ, .mainTB").removeClass().addClass("mainWK");
+    $(".authorPicHN, .authorPicJG, .authorPicWK, .authorPicTS, .authorPicV, .authorPicBQ, .authorPicTB").removeClass().addClass("authorPicWK");
+    $(".authorWordsHN, .authorWordsJG, .authorWordsWK, .authorWordsTS, .authorWordsV, .authorWordsBQ, .authorWordsTB").removeClass().addClass("authorWordsWK");
+    $(".shopHN, .shopJG, .shopWK, .shopTS, .shopV, .shopBQ, .shopTB").removeClass().addClass("shopWK");
+});
+$("#TS").click(function(){
+    $(".bodyHN, .bodyJG, .bodyWK, .bodyTS, .bodyV, .bodyBQ, .bodyTB").removeClass().addClass("bodyTS");
+    $(".topHN, .topJG, .topWK, .topTS, .topV, .topBQ, .topTB").removeClass().addClass("topTS");
+    $(".navHN, .navJG, .navWK, .navTS, .navV, .navBQ, .navTB").removeClass().addClass("navTS");
+    $(".buttonsHN, .buttonsJG, .buttonsWK, .buttonsTS, .buttonsV, .buttonsBQ, .buttonsTB").removeClass().addClass("buttonsTS");
+    $(".footHN, .footJG, .footWK, .footTS, .footV, .footBQ, .footTB").removeClass().addClass("footTS");
+    $(".smallTextHN, .smallTextJG, .smallTextWK, .smallTextTS, .smallTextV, .smallTextBQ, .smallTextTB").removeClass().addClass("smallTextTS");
+    $(".mainBioHN, .mainBioJG, .mainBioWK, .mainBioTS, mainBioV, mainBioBQ, mainBioTB").removeClass().addClass("mainBioTS");
+    $(".bioBtnHN, .bioBtnJG, .bioBtnWK, .bioBtnTS, .bioBtnV, .bioBtnBQ, .bioBtnTB").removeClass().addClass("bioBtnTS");
+    $(".mainHN, .mainJG, .mainWK, .mainTS, .mainV, .mainBQ, .mainTB").removeClass().addClass("mainTS");
+    $(".authorPicHN, .authorPicJG, .authorPicWK, .authorPicTS, .authorPicV, .authorPicBQ, .authorPicTB").removeClass().addClass("authorPicTS");
+    $(".authorWordsHN, .authorWordsJG, .authorWordsWK, .authorWordsTS, .authorWordsV, .authorWordsBQ, .authorWordsTB").removeClass().addClass("authorWordsTS");
+    $(".shopHN, .shopJG, .shopWK, .shopTS, .shopV, .shopBQ, .shopTB").removeClass().addClass("shopTS");
+});
+$("#V").click(function(){
+    $(".bodyHN, .bodyJG, .bodyWK, .bodyTS, .bodyV, .bodyBQ, .bodyTB").removeClass().addClass("bodyV");
+    $(".topHN, .topJG, .topWK, .topTS, .topV, .topBQ, .topTB").removeClass().addClass("topV");
+    $(".navHN, .navJG, .navWK, .navTS, .navV, .navBQ, .navTB").removeClass().addClass("navV");
+    $(".buttonsHN, .buttonsJG, .buttonsWK, .buttonsTS, .buttonsV, .buttonsBQ, .buttonsTB").removeClass().addClass("buttonsV");
+    $(".footHN, .footJG, .footWK, .footTS, .footV, .footBQ, .footTB").removeClass().addClass("footV");
+    $(".smallTextHN, .smallTextJG, .smallTextWK, .smallTextTS, .smallTextV, .smallTextBQ, .smallTextTB").removeClass().addClass("smallTextV");
+    $(".mainBioHN, .mainBioJG, .mainBioWK, .mainBioTS, mainBioV, mainBioBQ, mainBioTB").removeClass().addClass("mainBioV");
+    $(".bioBtnHN, .bioBtnJG, .bioBtnWK, .bioBtnTS, .bioBtnV, .bioBtnBQ, .bioBtnTB").removeClass().addClass("bioBtnV");
+    $(".mainHN, .mainJG, .mainWK, .mainTS, .mainV, .mainBQ, .mainTB").removeClass().addClass("mainV");
+    $(".authorPicHN, .authorPicJG, .authorPicWK, .authorPicTS, .authorPicV, .authorPicBQ, .authorPicTB").removeClass().addClass("authorPicV");
+    $(".authorWordsHN, .authorWordsJG, .authorWordsWK, .authorWordsTS, .authorWordsV, .authorWordsBQ, .authorWordsTB").removeClass().addClass("authorWordsV");
+    $(".shopHN, .shopJG, .shopWK, .shopTS, .shopV, .shopBQ, .shopTB").removeClass().addClass("shopV");
+});
+$("#BQ").click(function(){
+    $(".bodyHN, .bodyJG, .bodyWK, .bodyTS, .bodyV, .bodyBQ, .bodyTB").removeClass().addClass("bodyBQ");
+    $(".topHN, .topJG, .topWK, .topTS, .topV, .topBQ, .topTB").removeClass().addClass("topBQ");
+    $(".navHN, .navJG, .navWK, .navTS, .navV, .navBQ, .navTB").removeClass().addClass("navBQ");
+    $(".buttonsHN, .buttonsJG, .buttonsWK, .buttonsTS, .buttonsV, .buttonsBQ, .buttonsTB").removeClass().addClass("buttonsBQ");
+    $(".footHN, .footJG, .footWK, .footTS, .footV, .footBQ, .footTB").removeClass().addClass("footBQ");
+    $(".smallTextHN, .smallTextJG, .smallTextWK, .smallTextTS, .smallTextV, .smallTextBQ, .smallTextTB").removeClass().addClass("smallTextBQ");
+    $(".mainBioHN, .mainBioJG, .mainBioWK, .mainBioTS, mainBioV, mainBioBQ, mainBioTB").removeClass().addClass("mainBioBQ");
+    $(".bioBtnHN, .bioBtnJG, .bioBtnWK, .bioBtnTS, .bioBtnV, .bioBtnBQ, .bioBtnTB").removeClass().addClass("bioBtnBQ");
+    $(".mainHN, .mainJG, .mainWK, .mainTS, .mainV, .mainBQ, .mainTB").removeClass().addClass("mainBQ");
+    $(".authorPicHN, .authorPicJG, .authorPicWK, .authorPicTS, .authorPicV, .authorPicBQ, .authorPicTB").removeClass().addClass("authorPicBQ");
+    $(".authorWordsHN, .authorWordsJG, .authorWordsWK, .authorWordsTS, .authorWordsV, .authorWordsBQ, .authorWordsTB").removeClass().addClass("authorWordsBQ");
+    $(".shopHN, .shopJG, .shopWK, .shopTS, .shopV, .shopBQ, .shopTB").removeClass().addClass("shopBQ");
+});
+$("#TB").click(function(){
+    $(".bodyHN, .bodyJG, .bodyWK, .bodyTS, .bodyV, .bodyBQ, .bodyTB").removeClass().addClass("bodyTB");
+    $(".topHN, .topJG, .topWK, .topTS, .topV, .topBQ, .topTB").removeClass().addClass("topTB");
+    $(".navHN, .navJG, .navWK, .navTS, .navV, .navBQ, .navTB").removeClass().addClass("navTB");
+    $(".buttonsHN, .buttonsJG, .buttonsWK, .buttonsTS, .buttonsV, .buttonsBQ, .buttonsTB").removeClass().addClass("buttonsTB");
+    $(".footHN, .footJG, .footWK, .footTS, .footV, .footBQ, .footTB").removeClass().addClass("footTB");
+    $(".smallTextHN, .smallTextJG, .smallTextWK, .smallTextTS, .smallTextV, .smallTextBQ, .smallTextTB").removeClass().addClass("smallTextTB");
+    $(".mainBioHN, .mainBioJG, .mainBioWK, .mainBioTS, mainBioV, mainBioBQ, mainBioTB").removeClass().addClass("mainBioTB");
+    $(".bioBtnHN, .bioBtnJG, .bioBtnWK, .bioBtnTS, .bioBtnV, .bioBtnBQ, .bioBtnTB").removeClass().addClass("bioBtnTB");
+    $(".mainHN, .mainJG, .mainWK, .mainTS, .mainV, .mainBQ, .mainTB").removeClass().addClass("mainTB");
+    $(".authorPicHN, .authorPicJG, .authorPicWK, .authorPicTS, .authorPicV, .authorPicBQ, .authorPicTB").removeClass().addClass("authorPicTB");
+    $(".authorWordsHN, .authorWordsJG, .authorWordsWK, .authorWordsTS, .authorWordsV, .authorWordsBQ, .authorWordsTB").removeClass().addClass("authorWordsTB");
+    $(".shopHN, .shopJG, .shopWK, .shopTS, .shopV, .shopBQ, .shopTB").removeClass().addClass("shopTB");
+});
+
+$(document).ready(function(){
+    $(".charBio").hide();
+});
+$("#JGbtn").click(function(){
+    $("#JGBio").toggle();
+});
+$("#WKbtn").click(function(){
+    $("#WKBio").toggle();
+});
+$("#TSbtn").click(function(){
+    $("#TSBio").toggle();
+});
+$("#Vbtn").click(function(){
+    $("#VBio").toggle();
+});
+$("#BQbtn").click(function(){
+    $("#BQBio").toggle();
+});
+$("#TBbtn").click(function(){
+    $("#TBBio").toggle();
+});
+$(".charBio").click(function(){
+    $(this).hide();
+})
